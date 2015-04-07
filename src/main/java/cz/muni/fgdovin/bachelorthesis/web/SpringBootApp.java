aeb6e7338556280f4c84a8e534f5ebc22ee7e528
f-gdovin <f.gdovin@gmail.com>
2015-04-07 21:42:14 +0200

    15.04.07

--- src/main/java/cz/muni/fgdovin/bachelorthesis/web/SpringBootApp.java
+++ src/main/java/cz/muni/fgdovin/bachelorthesis/web/SpringBootApp.java
@@ -5,9 +5,6 @@ import cz.muni.fgdovin.bachelorthesis.core.EsperServiceImpl;
 import cz.muni.fgdovin.bachelorthesis.core.EsperUserFriendlyService;
 import cz.muni.fgdovin.bachelorthesis.core.EsperUserFriendlyServiceImpl;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.espertech.esper.client.*;
 import com.espertech.esper.client.dataflow.EPDataFlowRuntime;
 import com.espertech.esperio.amqp.AMQPSink;
@@ -16,19 +13,12 @@ import com.espertech.esperio.amqp.AMQPSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.ViewResolver;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.view.ThymeleafViewResolver;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
-
 
 /**
  * Created by Filip Gdovin on 4. 3. 2015.
  */
 @SpringBootApplication
 public class SpringBootApp {
-
-    private static final Logger logger = LogManager.getLogger("MvcConfig");
 
     private EPServiceProvider epServiceProvider;
     private EPRuntime epRuntime;
@@ -66,20 +56,6 @@ public class SpringBootApp {
     public ConfigurationOperations configurationOperations() {
         return epServiceProvider().getEPAdministrator().getConfiguration();
     }
-
-    /*@Bean
-    public ViewResolver viewResolver() {
-        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-        templateResolver.setTemplateMode("HTML5");
-        templateResolver.setPrefix("/WEB-INF/templates/");
-        templateResolver.setSuffix(".html");
-        SpringTemplateEngine engine = new SpringTemplateEngine();
-        engine.setTemplateResolver(templateResolver);
-
-        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-        viewResolver.setTemplateEngine(engine);
-        return viewResolver;
-    }*/
 
     @Bean
     public EsperService esperService() {
