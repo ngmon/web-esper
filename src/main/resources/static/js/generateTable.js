5e53d2c72ddd0a578457c4c58eec5d00164476b2
f-gdovin <f.gdovin@gmail.com>
2015-05-05 15:01:57 +0200

    15.05.05-v3
    - rewritten javascript script to be able to dynamically fill
    table with correct values
    - addInputDataflow surprisingly works as intended... but not with Map

--- src/main/resources/static/js/generateTable.js
+++ src/main/resources/static/js/generateTable.js
@@ -7,11 +7,62 @@ function paintTable(){
 
     function successCallBack(data) {
         $("#dyntable").find("tbody").empty();
+
+        if(data == null) {
+            return;
+        }
+        var i = 0;
         $.each(data, function (k, v) {
-            var newRowContent = "<tr><td><input type=\"checkbox\" checked=\"true\"></td>" +
-                "<td>" + k + "</td>" +
-                "<td>" + v + "</td></tr>";
+            var newRowContent = '<tr class="oneProperty">' +
+                '<td><input type="checkbox" class="selectedProp" onclick="sortIds()" checked></td>';
+
+            var id = "mapProperties" + i + ".key";
+            var arrayId = "mapProperties[" + i + "].key";
+            newRowContent += "<td><input readonly type=\"text\" class=\"propertyName\" id=\"" + id + "\" name=\"" + arrayId + "\" value=\"" + k + "\"></td>";
+
+            id = "mapProperties" + i + ".value";
+            arrayId = "mapProperties[" + i + "].value";
+            newRowContent += ("<td><input readonly type=\"text\" class=\"propertyType\" id=\"" + id + "\" name=\"" + arrayId + "\" value=\"" + v + "\"></td>");
+
             $("#dyntable").find("tbody").append(newRowContent);
+            i++;
         });
     }
+}
+
+function sortIds(){
+    var index = 0;
+    $('#dyntable').find('tbody').find('tr').each(function() {
+        var checkBoxChecked = $(this).find("input[class=selectedProp]")[0].checked;
+        var propName = $(this).find("input[class=propertyName]")[0];
+        var propType = $(this).find("input[class=propertyType]")[0];
+
+        var id;
+        var name;
+
+        if(checkBoxChecked) {
+            id = "mapProperties" + index + ".key";
+            name = "mapProperties[" + index + "].key";
+
+            propName.id = id;
+            propName.name = name;
+
+            id = "mapProperties" + index + ".value";
+            name = "mapProperties[" + index + "].value";
+
+            propType.id = id;
+            propType.name = name;
+
+            index++;
+        } else {
+            id = "meh";
+            name = "nah";
+
+            propName.id = id;
+            propName.name = name;
+
+            propType.id = id;
+            propType.name = name;
+        }
+    });
 }
