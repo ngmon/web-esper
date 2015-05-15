5db7bfc4ad21a9a7ed1d184a5947e2122bc90d83
f-gdovin <f.gdovin@gmail.com>
2015-05-15 11:11:18 +0200

    15.05.15
    -RESTApi
    logger added

--- src/main/java/cz/muni/fgdovin/bachelorthesis/web/RESTApi.java
+++ src/main/java/cz/muni/fgdovin/bachelorthesis/web/RESTApi.java
@@ -3,6 +3,8 @@ package cz.muni.fgdovin.bachelorthesis.web;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import cz.muni.fgdovin.bachelorthesis.rabbit.RabbitMqService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.core.env.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,8 @@ import javax.annotation.Resource;
 @RestController
 public class RESTApi {
 
+    private static final Logger logger = LogManager.getLogger("RESTApi");
+
     @Resource
     Environment environment;
 
@@ -27,14 +31,18 @@ public class RESTApi {
     @RequestMapping("/getSchema")
     public String getSchema(@RequestParam(value="name") String name) {
         if(!(name.startsWith(this.environment.getProperty("exchangePrefix")))) {
+            logger.warn("Bad prefix :/, returning null schema for " + name);
             return null;
         }
         ObjectMapper mapper = new ObjectMapper();
         String result = null;
         try {
             result = mapper.writeValueAsString(this.rabbitMqService.getSchemaForExchange(name));
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            logger.error("Failed to get schema for exchange " + name + ", returning " + result);
+        }
+        if(logger.isDebugEnabled()){
+            logger.debug("Returning " + result + " as schema for " + name);
         }
         return result;
     }
